86d03f8cbf336f7d75089a48d50e83240a1bfe30
Admin <Admin@DESKTOP-17CHQC0>
2023-05-03 14:26:08 +0800

    Altangerel

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		Course course = new Course("Software Construction");
+		
 		course.setCourseName("Software Construction");
 		System.out.println(course.getCourseName());
 		course.setCourseName("Программ хангамжийн бүтээлт");
 		System.out.println(course.getCourseName());
 	}
-
 }
